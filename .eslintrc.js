a5fe0ecb850c0c2a04b4c8ca0a3186685a501a89
TuanBlue980 <tuanmeo980provip@gmail.com>
2025-05-29 13:59:45 +0700

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,22 @@
 module.exports = {
   extends: ['next/core-web-vitals'],
-  ignorePatterns: [
-    '**/node_modules/**',
-    '**/.next/**',
-    '**/out/**',
-    '**/build/**',
-    '**/generated/**',
-    'src/generated/**',
-    '**/prisma/generated/**',
-    '**/prisma/client.js',
-    '**/prisma/edge.js',
-    '**/prisma/default.js'
-  ],
-  rules: {
-    '@typescript-eslint/no-unused-expressions': 'off',
-    '@typescript-eslint/no-require-imports': 'off',
-    '@typescript-eslint/no-unused-vars': 'off'
-  }
+  ignorePatterns: ['**/*'],
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx', '**/*.js'],
+      excludedFiles: [
+        '**/generated/**',
+        '**/prisma/generated/**',
+        '**/prisma/client.js',
+        '**/prisma/edge.js',
+        '**/prisma/default.js',
+        'src/generated/**'
+      ],
+      rules: {
+        '@typescript-eslint/no-unused-expressions': 'error',
+        '@typescript-eslint/no-require-imports': 'error',
+        '@typescript-eslint/no-unused-vars': 'error'
+      }
+    }
+  ]
 } 
